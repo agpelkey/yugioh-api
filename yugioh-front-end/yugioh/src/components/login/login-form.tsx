ba32acb82aae9dc2196d59686280884a6052c1e1
Alex Pelkey <agpelkey@yahoo.com>
2023-02-20 20:08:46 -0800

    continue work on login page

--- yugioh-front-end/yugioh/src/components/login/login-form.tsx
+++ yugioh-front-end/yugioh/src/components/login/login-form.tsx
@@ -2,28 +2,46 @@ import Head from "next/head";
 
 function LoginForm() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100">
+    <div className="flex flex-col items-center justify-center min-h-screen py-3 bg-gray-100">
       <Head>
         <title>Login</title>
       </Head>
 
+      <h1 className="flex flex-col mt-20">Login</h1>
       <main className="flex-flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <div className="w-3/5 p-10"></div>
+        <div className="w-3/5"></div>
         <div className="mb-10"></div>
-        <form className="space-x-3" action="http://localhost:8080/login" method="post">
+        <form
+          className="space-x-3"
+          action="http://localhost:8080/login"
+          method="post"
+        >
           <label htmlFor="username">Username</label>
 
-          <input className="mt-6 outline outline-offset-2 outline-1 shadow-xl" type="text" id="username" name="username" required />
+          <input
+            className="outline outline-offset-2 outline-1 shadow-xl"
+            type="text"
+            id="username"
+            name="username"
+            required
+          />
           <form className="space-x-3">
             <label htmlFor="password">Password</label>
-            <input className="mt-6 outline outline-offset-2 outline-1 shadow-xl" type="text" id="paswword" name="password" required />
-
-            <button
-              className="w-3/5 py-2 mt-6 text-medium text-white bg-blue-500 rounded-md"
-              type="submit"
-            >
-              Submit
-            </button>
+            <input
+              className="mt-6 outline outline-offset-2 outline-1 shadow-xl"
+              type="text"
+              id="paswword"
+              name="password"
+              required
+            />
+            <li className="list-none">
+              <button
+                className="w-1/3 bg-orange-200 hover:bg-orange-400 rounded-lg my-4"
+                type="submit"
+              >
+                Submit
+              </button>
+            </li>
           </form>
         </form>
       </main>
