f3beff30e5d50810cd18011b7388ad87069042f5
Alex Pelkey <agpelkey@yahoo.com>
2023-03-06 21:08:35 -0800

    fixed about page div

--- yugioh-front-end/yugioh/src/pages/about.js
+++ yugioh-front-end/yugioh/src/pages/about.js
@@ -3,22 +3,20 @@ import DMG from "../../public/DMG.webp";
 
 const About = () => {
   return (
-    <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800:">
-      <div className="flex flex-wrap justify-center-left">
-        <div className="w-6/12 sm:w-4/12 px-4">
+    <div className="flex flex-row bg-slate-100 rounded-xl p-8 dark:bg-slate-800:">
+        <div className="flex basis-1/4">
           <Image
             src={DMG}
             alt="/"
-            className="shadow rounded-full max-w-full h-auto align-middle border-none"
+            className="max-h-full w-3/4 px-4 shadow rounded-full h-auto align-middle border-none"
           />
-          <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+            </div>
+          <div className="basis-1/4">
             <blockquote>
               <p className="text-lg font-medium">"INSERT ABOUT INFO HERE"</p>
             </blockquote>
           </div>
         </div>
-      </div>
-    </figure>
   );
 };
 
